f6ecf4f47020a0579f917ee57b8eaea469d10366
Jacob Lee <wlee33@u.rochester.edu>
2019-02-22 17:00:38 -0500

    Updated index.php to the homepage

--- index.php
+++ index.php
@@ -1,50 +1,68 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <?php include 'includes/head.php';?>
-    <title>Home | Lab 2: Flash Website</title>   
+	<?php include 'includes/head.php';?>
+	<title> Odd Student Out </title>
 </head>
 
 <body class="container">
+	<header>
+		<a href="index.php">
+			<h1>Lab 02</h1>
+		</a>
+	</header>
+	<main>
+		<article>
+			<a href="index.php">
+				<h1>Giovanni Ilacqua</h1>
+				<img class="profile-picture" src="images/profile_photo.jpg" alt="A profile photo">
+			</a>
+			<aside>
+				<ul>
+					<li>Computer Science Major</li>
+					<li>Qualified Fitness Instructor</li>
+					<li>Graphic Designer</li>
+				</ul>
+			</aside>
+		</article>
+		<article>
+			<a href="index.php">
+				<h1>Genessis Galindo</h1>
+				<img class="profile-picture" src="images/genessis.jpg" alt="Genessis Galindo">
+			</a>
 
-    <header>
-        <a href="index.php">
-            <h1>Lab 02</h1>
-        </a>
-    </header>
+			<aside>
+				<ul>
+					<li>Wannabe Master of Web Development</li>
+					<li>Ninja-level Event Planning Skills </li>
+					<li>Undergraduate Student at University of Rochester</li>
+				</ul>
+			</aside>
+		</article>
+		<article>
+			<a href="index.php">
+				<h1>Jack Dalton</h1>
+				<img class="profile-picture" src="images/jack_clubs.jpg" alt="Jack">
+			</a>
 
-    <article>
-        <h2>Giovanni Ilacqua</h2>
-        <p>I was born in Sicily in the city of Messina but I moved to Milan when I was 3 years old. After studying Information
-            Technology and Science at high school, I decided I wanted to attend University abroad. Originally I intended to
-            study in the US but I then decided to apply to universities in the UK, because studying in America was extremely
-            expensive. At 19 I therefore moved to England to study Computer Science at the University of York.
-            I studied 2 years at York and although the campus and the courses were great I still had the dream to study in the
-            US!
-            So I applied to the exchange program for North America and I managed to be selected for the University of
-            Rochester.</p>
-    </article>
-
-    <article>
-        <h2>Genessis Galindo</h2>
-
-        <p>I am a student majoring in Business, with a concentration in Analytics, and minoring in Computer Science and Spanish. Before coming to the University of Rochester, I attended Canyon High School in California where I was involved in various extracurricular such as: Student Government, National Honor Society, English National Honor Society, Spirit Club, Chemistry Club, and a few others. I even founded a chapter of the Science National Honor Society at my school and at the time I believed I wanted to major in Chemistry.</p>
-    </article>
-
-    <article>
-        <h2>Jack Dalton</h2>
-        <p> I am a Mechanical Engineering major at the University of Rochester, and I am also minoring in Computer Science. I am very interested in how the world works, and I find both theoretical research and its application interesting. However, I decided to major in Mechanical Engineering because the idea of building something that could impact others interests me, which is why I also decided to minor in computer science. So far at UofR, I have taken the following courses: CSC 170, CSC 171, CSC 172, CSC 214, and CSC 240. Additionally, I have been a CHM 132 Lab Teacher Assistant, and I am currently a CSC 240/440 TA.</p>
-    </article>
-
-    <?php include 'includes/nav.php';?>
+			<aside>
+				<ul>
+					<li>Mechanical Engineering major</li>
+					<li>Computer Science minor</li>
+					<li>Graduating in 2020</li>
+				</ul>
+			</aside>
+		</article>
+	</main>
+	<?php include 'includes/nav.php';?>
 
 
-    <footer>
-        CSC 174: Advanced Front-end Web Design and Development
-    </footer>
+	<footer>
+		CSC 174: Advanced Front-end Web Design and Development
+	</footer>
 
-    <?php include 'includes/scripts.php';?>
+	<?php include 'includes/scripts.php';?>
 </body>
 
 
